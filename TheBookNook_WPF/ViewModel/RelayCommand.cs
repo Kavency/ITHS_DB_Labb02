e0294c1d0e24c5727e1deaa85a9a5d5b666aa326
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-04 17:50:34 +0100

    Add RelayCommand class

---
+++ TheBookNook_WPF/ViewModel/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TheBookNook_WPF.ViewModel
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<object> _executeCommand;
+        private readonly Predicate<object> _canExecuteCommand;
+
+        public RelayCommand(Action<object> executeCommand)
+        {
+            _executeCommand = executeCommand;
+            _canExecuteCommand = null;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecuteCommand == null ? true : _canExecuteCommand(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeCommand(parameter);
+        }
+    }
+}
