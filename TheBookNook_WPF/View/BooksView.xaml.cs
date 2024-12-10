7e3cbde8a4afca9f88af76b4766d1300f1586742
Magnus Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-10 15:54:19 +0100

    Add viewmodels

---
+++ TheBookNook_WPF/View/BooksView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using TheBookNook_WPF.ViewModel;
+
+namespace TheBookNook_WPF.View
+{
+    /// <summary>
+    /// Interaction logic for BooksDataGrid.xaml
+    /// </summary>
+    public partial class BooksView : UserControl
+    {
+        public BooksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TheBookNook_WPF/ViewModel/MainWindowVM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TheBookNook_WPF.Model;
+using TheBookNook_WPF.View;
+
+namespace TheBookNook_WPF.ViewModel
+{
+    public class MainWindowVM : VMBase
+    {
+        public ObservableCollection<Book> Books { get; set; } = new();
+
+        public Visibility BookViewVisibility { get; set; } = Visibility.Collapsed;
+        public ICommand ShowBookViewCMD { get; set; }
+
+        public MainWindowVM()
+        {
+            GetBooks();
+
+            ShowBookViewCMD = new RelayCommand(ShowBookView, CanShowBookView);
+        }
+
+        private bool CanShowBookView(object obj)
+        {
+            return true;
+        }
+
+        private void ShowBookView(object obj)
+        {
+            BooksView BooksDG = new BooksView();
+            BookViewVisibility = Visibility.Visible;
+        }
+
+        private void GetBooks() 
+        {
+            using var db = new TheBookNookDbContext();
+            var books = db.Books.ToList();
+            
+            foreach (var item in books)
+            {
+                this.Books.Add(item);
+            }
+        }
+    }
+}
