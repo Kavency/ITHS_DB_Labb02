786fbfe5a02f32a58b54203d25cbf8e998359798
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-17 14:19:40 +0100

    Cleanup

--- TheBookNook_WPF/ViewModel/BooksVM.cs
+++ TheBookNook_WPF/ViewModel/BooksVM.cs
@@ -44,7 +44,7 @@ namespace TheBookNook_WPF.ViewModel
             var data = await Task.Run(() => GetDataFromDatabase());
 
             Books = data;
-            OnPropertyChanged("Books");
+            OnPropertyChanged(nameof(Books));
         }
 
 
@@ -52,24 +52,7 @@ namespace TheBookNook_WPF.ViewModel
         {
             using var db = new TheBookNookDbContext();
             var books = new ObservableCollection<Book>(db.Books.ToList());
-
-            //var query = from book in db.Books
-            //                //from author in db.Authors
-
-            //            join bookAuthor in db.Books on book.Isbn equals bookAuthor.Isbn
-            //            //join author in db.Authors on author.Id equals author2.Id
-            //            //join bookAuthor in db.Books on book.Authors equals 
-            //            //join bookisbn in db.Authors on b
-            //            select new
-            //            {
-            //                book.Isbn,
-            //                book.Title
-            //                //author.FirstName,
-            //                //author.LastName
-            //            };
-
-            //var someList = query.ToList();
-
+            
             return books;
         }
     }
