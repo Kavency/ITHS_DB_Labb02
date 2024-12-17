a88b17b2549d0299cd48568bb885f0137b691846
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-17 14:16:24 +0100

    Add String to Int converter

---
+++ TheBookNook_WPF/Converters/StringToIntConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace TheBookNook_WPF.Converters
+{
+    internal class StringToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (int.TryParse(value as string, out int result))
+            {
+                return result;
+            }
+
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+    }
+}
