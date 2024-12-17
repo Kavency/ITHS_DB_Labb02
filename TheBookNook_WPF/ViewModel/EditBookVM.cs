469856d2f400077e21d922ae7cd080621ce426bf
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-17 19:18:38 +0100

    Add ViewModel

---
+++ TheBookNook_WPF/ViewModel/EditBookVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBookNook_WPF.Model;
+
+namespace TheBookNook_WPF.ViewModel
+{
+    internal class EditBookVM
+    {
+        private ObservableCollection<Author> Authors;
+    }
+}
