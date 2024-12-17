c3a71c68793416ae1fe640b42d47bd31b4e0561f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-17 18:05:18 +0100

    Add usercontrols

---
+++ TheBookNook_WPF/View/AddBookPane.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TheBookNook_WPF.View
+{
+    /// <summary>
+    /// Interaction logic for AddBookPane.xaml
+    /// </summary>
+    public partial class AddBookPane : UserControl
+    {
+        public AddBookPane()
+        {
+            InitializeComponent();
+        }
+    }
+}
