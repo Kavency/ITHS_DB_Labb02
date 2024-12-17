bce4da743ae739b5c7f47836e8679db58c8e436d
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-17 14:17:15 +0100

    Add validation

---
+++ TheBookNook_WPF/ValidationRules/NotEmptyValidation.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace TheBookNook_WPF.ValidationRules
+{
+    internal class NotEmptyValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(string.IsNullOrWhiteSpace(value as string))
+                return new ValidationResult(false, "Field cannot be empty.");
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
