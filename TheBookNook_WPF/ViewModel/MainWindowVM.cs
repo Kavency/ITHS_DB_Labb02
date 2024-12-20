c530e19c9a8d911f33129088f9a469c7c4b37574
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-21 00:29:00 +0100

    Refactor ViewModel by move code to a different VM

--- TheBookNook_WPF/ViewModel/MainWindowVM.cs
+++ TheBookNook_WPF/ViewModel/MainWindowVM.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows;
-using TheBookNook_WPF.Model;
-
-namespace TheBookNook_WPF.ViewModel
+﻿namespace TheBookNook_WPF.ViewModel
 {
     public class MainWindowVM : VMBase
     {
         #region Fields
         private object _currentView;
         private bool _sideMenuIsEnabled = true;
-        private ObservableCollection<Author>? _authors;
-        private Visibility _dimBackgroundVisibility = Visibility.Hidden;
         #endregion
 
 
@@ -26,8 +20,6 @@ namespace TheBookNook_WPF.ViewModel
         public RelayCommand ShowAuthorsViewCMD { get; set; }
         public RelayCommand ShowCustomersViewCMD { get; set; }
         public RelayCommand ShowStoresViewCMD { get; set; }
-        public ObservableCollection<Author>? Authors { get => _authors; set { _authors = value; OnPropertyChanged(); } }
-
         #endregion
 
 
@@ -36,9 +28,7 @@ namespace TheBookNook_WPF.ViewModel
             this.HomeVM = new HomeVM();
             this.BooksVM = new BooksVM(this);
             this.AuthorsVM = new AuthorsVM(this);
-            this.StoresVM = new StoresVM();
-
-            _authors = new ObservableCollection<Author>();
+            this.StoresVM = new StoresVM(this);
 
             CurrentView = HomeVM;
 
@@ -46,39 +36,14 @@ namespace TheBookNook_WPF.ViewModel
             ShowBookViewCMD = new RelayCommand(x => { CurrentView = BooksVM; ClearAllSelected(); });
             ShowAuthorsViewCMD = new RelayCommand(x => { CurrentView = AuthorsVM; ClearAllSelected(); });
             ShowStoresViewCMD = new RelayCommand(x => { CurrentView = StoresVM; ClearAllSelected(); });
-
-            LoadAuthorsAsync();
-            AuthorFullName();
         }
 
         #region Methods
-        private async void LoadAuthorsAsync()
-        {
-            var data = await Task.Run(() => GetDataFromDatabase());
-
-            Authors = data;
-            OnPropertyChanged(nameof(Authors));
-        }
-
-        private ObservableCollection<Author> GetDataFromDatabase()
-        {
-            using var db = new TheBookNookDbContext();
-            var authors = new ObservableCollection<Author>(db.Authors.ToList());
-
-            return authors;
-        }
-
-        private void AuthorFullName()
-        {
-            foreach (var author in Authors)
-            {
-                author.FullName = author.FirstName + " " + author.LastName;
-            }
-        }
-
         private void ClearAllSelected()
         {
             BooksVM.CurrentBook = null;
+            AuthorsVM.CurrentAuthor = null;
+            StoresVM.SelectedStore = null;
         }
         #endregion
     }
