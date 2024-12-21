66f15bce3fcc0d78f96ee310e1ed4fe94bb20e86
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-22 00:44:33 +0100

    Add entity model

---
+++ TheBookNook_WPF/Model/AuthorBook.cs
@@ -0,0 +1,10 @@
+﻿namespace TheBookNook_WPF.Model;
+
+public partial class AuthorBook
+{
+    public int AuthorId { get; set; }
+    public Author Author { get; set; }
+
+    public long Isbn { get; set; }
+    public Book Book { get; set; }
+}
