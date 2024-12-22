475c54cb1287e235fc6bd4f613718109a5a111be
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-22 19:48:36 +0100

    Add StoreBook model

---
+++ TheBookNook_WPF/Model/StoreBook.cs
@@ -0,0 +1,10 @@
+﻿namespace TheBookNook_WPF.Model;
+
+internal class StoreBook
+{
+    public int StoreId { get; set; }
+    public Store Store { get; set; }
+
+    public long Isbn { get; set; }
+    public Book Book { get; set; }
+}
