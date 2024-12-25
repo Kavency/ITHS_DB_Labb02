3b3a394b2350757e73412b8c2d5d471b8efd546e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-25 14:21:09 +0100

    Add converter to enable save button.

---
+++ TheBookNook_WPF/Converters/ValidationToBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace TheBookNook_WPF.Converters
+{
+    internal class ValidationToBoolConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool hasError = (bool)values[0];
+            string text = values[1] as string;
+            return !hasError && !string.IsNullOrEmpty(text);
+
+
+        }
+
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
