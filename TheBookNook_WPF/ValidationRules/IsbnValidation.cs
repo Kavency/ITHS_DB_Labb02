64faa921183b04e49cd199a2e3ee879adf7002f8
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-12-25 14:20:50 +0100

    Set up validation for isbn

---
+++ TheBookNook_WPF/ValidationRules/IsbnValidation.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace TheBookNook_WPF.ValidationRules
+{
+
+    public class IsbnValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) 
+        { 
+            string input = value as string; 
+            if (string.IsNullOrWhiteSpace(input)) 
+            { 
+                return new ValidationResult(false, "ISBN cannot be empty."); 
+            } 
+            Regex regex = new Regex(@"^\d{10}(\d{3})?$"); 
+            if (!regex.IsMatch(input)) 
+            { 
+                return new ValidationResult(false, "ISBN must be 10 or 13 digits.");
+            } 
+            return ValidationResult.ValidResult; 
+        }
+    }
+}
